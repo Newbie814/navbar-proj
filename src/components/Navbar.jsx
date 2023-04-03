131c452dadc15f85a61d05d94de8eea2226a63db
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-03 00:47:40 -0400

    setup initial content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,50 @@
+import { useState, useRef } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from '../data';
+
+const logoUrl1 =
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1656892877/website%20pics%20family/development_logo_hpyyun.jpg';
+
+const logoUrl2 =
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1647322071/website%20pics%20family/logo-banner_js0mkp.jpg';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img
+            src={logoUrl1}
+            alt='Woodard Solutions'
+            style={{ width: '18rem' }}
+          />
+        </div>
+      </div>
+      <div className='nav-links'>
+        <ul>
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className='social-icons'>
+        <ul>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
