1a413461de4b18f23e7117682f32fd20bd7160d9
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-03 00:32:07 -0400

    initial component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { useState, useRef } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+
+const logoUrl1 =
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1656892877/website%20pics%20family/development_logo_hpyyun.jpg';
+
+const logoUrl2 =
+  'https://res.cloudinary.com/dylvkdabj/image/upload/v1647322071/website%20pics%20family/logo-banner_js0mkp.jpg';
+
+import Navbar from './components/Navbar';
 const App = () => {
-  return <h2>Navbar Starter</h2>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 export default App;
